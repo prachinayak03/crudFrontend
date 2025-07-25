5c4cc78f05030cd6a666e4de3854b34ccb27eb77
prachinayak03 <nprachi860@gmail.com>
2025-07-25 18:30:34 +0530

    Update Update.jsx

--- src/Component/Update.jsx
+++ src/Component/Update.jsx
@@ -10,7 +10,7 @@ const Update = () => {
   const navigate = useNavigate()
 
   useEffect(()=>{
-    axios.get('http://localhost:3001/getUser/'+id)
+    axios.get('https://crud-backend-olive-alpha.vercel.app/getUser/'+id)
     .then(result => {console.log(result)
       setName(result.data.name)
       setEmail(result.data.email)
